d0825feb00eab3df49a2fbb88a3a4d0aa4c01998
Hoang Quoc Dung <hqdtech@gmail.com>
2012-10-27 12:18:02 +0700

    Edit my mistype

---
+++ Coding/Skin Implementation Document/web_root/application/views/skins/template_main_footer.php
@@ -0,0 +1,19 @@
+<footer class="cyc-footer clearfix">
+	<div class="cyc-footer-inner">
+		<div
+			style="position: relative; display: inline-block; padding-left: 42px; padding-right: 42px">
+			<a title="RSS" class="cyc-rss-tag-icon"
+				style="position: absolute; bottom: -10px; left: -6px; line-height: 32px;"
+				href="#"></a>
+			<p>
+				<a href="#">About</a> | <a href="http://www.Tutorial9.net/">LogoImage
+					by Tutorial9</a> | <a href="#">Terms</a>
+			</p>
+			<p>Copyright © 2011-2012. All Rights Reserved.</p>
+		</div>
+		<p class="cyc-page-footer">
+			<span id="cyc-footnote-links"><a href="http://www.artisteer.com/"
+				target="_blank">Web Template</a> created with Artisteer.</span>
+		</p>
+	</div>
+</footer>
